588f60df19058b66390b5d12025cef6bf17ae1a4
Jianfeng Li <lee_jianfeng@sjtu.edu.cn>
2020-04-25 13:19:42 +0800

    update 2_extrat.R

--- 2_extract.R
+++ 2_extract.R
@@ -16,9 +16,23 @@ if (!file.exists("cor/raw.json")) {
 ct <- paste0(c(countries, cities), "[^0-9a-zA-Z]")
 cat(ct, file = "/tmp/bioextr.kws", sep = "\n")
 
-#if (!file.exists("cor/abs_countries_cities.json")) {
+if (!file.exists("cor/abs_countries_cities.json")) {
   cmd = sprintf("bioextr --mode pubmed --call-cor --keywords-file /tmp/bioextr.kws -t 80 xml/*.json > cor/abs_countries_cities.json")
   system(cmd)
-#}
-#writeLines(as.character(dat[dat[,1] != "",1]), "id/20200423.pmid")
-#writeLines(as.character(dat[dat[,2] != "",2]), "id/20200423.doi")
+}
+
+hgnc <- as.data.frame(fread("ref/hgnc-simple.txt"))
+alias_name <- unlist(str_split(hgnc[,"alias_name"], fixed("|")))
+alias_symbol <- unlist(str_split(hgnc[,"alias_symbol"], fixed("|")))
+ccds_id <- unlist(str_split(hgnc[,"ccds_id"], fixed("|")))
+uniprot_ids <- unlist(str_split(hgnc[,"uniprot_ids"], fixed("|")))
+refseq_accession <- unlist(str_split(hgnc[,"refseq_accession"], fixed("|")))
+
+kw <- paste0(c(hgnc$symbol, hgnc$name, hgnc$ensembl_gene_id, alias_name,
+               hgnc$ucsc_id, alias_symbol, ccds_id, uniprot_ids, refseq_accession), "[^0-9a-zA-Z]")
+cat(kw, file = "/tmp/bioextr.kws", sep = "\n")
+
+if (!file.exists("cor/abs_genes.json")) {
+  cmd = sprintf("bioextr --mode pubmed --call-cor --keywords-file /tmp/bioextr.kws -t 80 xml/*.json > cor/abs_genes.json")
+  system(cmd)
+}
