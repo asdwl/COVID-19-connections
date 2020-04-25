0db4f94bc3022315839207fdf9b1bf9594e513fe
Jianfeng Li <lee_jianfeng@sjtu.edu.cn>
2020-04-25 16:42:19 +0800

    add abs_genes.xlsx

--- 2_extract.R
+++ 2_extract.R
@@ -13,26 +13,41 @@ if (!file.exists("cor/raw.json")) {
   system(cmd)
 }
 
-ct <- paste0(c(countries, cities), "[^0-9a-zA-Z]")
-cat(ct, file = "/tmp/bioextr.kws", sep = "\n")
-
-if (!file.exists("cor/abs_countries_cities.json")) {
-  cmd = sprintf("bioextr --mode pubmed --call-cor --keywords-file /tmp/bioextr.kws -t 80 xml/*.json > cor/abs_countries_cities.json")
+if (!file.exists("cor/abs_countries_cities.xlsx")) {
+  ct <- paste0(c(countries, cities), "[^0-9a-zA-Z]")
+  cat(ct, file = "/tmp/bioextr.kws", sep = "\n")
+  cmd = sprintf("bioextr --mode pubmed --call-cor --keywords-file /tmp/bioextr.kws -t 20 xml/*.json > cor/abs_countries_cities.json")
   system(cmd)
 }
 
+if (!file.exists("cor/abs_genes.xlsx")) {
+if (!file.exists("cor/abs_genes.json")) {
+library(stringr)
 hgnc <- as.data.frame(fread("ref/hgnc-simple.txt"))
 alias_name <- unlist(str_split(hgnc[,"alias_name"], fixed("|")))
+alias_name <- alias_name[alias_name != ""]
 alias_symbol <- unlist(str_split(hgnc[,"alias_symbol"], fixed("|")))
+alias_symbol <- alias_symbol[alias_symbol != ""]
 ccds_id <- unlist(str_split(hgnc[,"ccds_id"], fixed("|")))
+ccds_id <- ccds_id[ccds_id != ""]
 uniprot_ids <- unlist(str_split(hgnc[,"uniprot_ids"], fixed("|")))
+uniprot_ids <- uniprot_ids[uniprot_ids != ""]
 refseq_accession <- unlist(str_split(hgnc[,"refseq_accession"], fixed("|")))
+refseq_accession <- refseq_accession[refseq_accession != ""]
 
-kw <- paste0(c(hgnc$symbol, hgnc$name, hgnc$ensembl_gene_id, alias_name,
-               hgnc$ucsc_id, alias_symbol, ccds_id, uniprot_ids, refseq_accession), "[^0-9a-zA-Z]")
+print(head(hgnc))
+
+#kw <- paste0(c(hgnc$symbol, hgnc$name, hgnc$ensembl_gene_id, alias_name,
+#               hgnc$ucsc_id, alias_symbol, ccds_id, uniprot_ids, refseq_accession), "[^0-9a-zA-Z]")
+kw <- paste0("[^0-9a-zA-Z]", c(hgnc$symbol, alias_symbol), "[^0-9a-zA-Z]")
 cat(kw, file = "/tmp/bioextr.kws", sep = "\n")
+cmd = sprintf("bioextr --mode pubmed --call-cor --keywords-file /tmp/bioextr.kws -t 20 xml/*.json > cor/abs_genes.json && json2csv -i cor/abs_genes.json -o cor/abs_genes.csv -q '`' ")
+system(cmd)
+}
 
-if (!file.exists("cor/abs_genes.json")) {
-  cmd = sprintf("bioextr --mode pubmed --call-cor --keywords-file /tmp/bioextr.kws -t 80 xml/*.json > cor/abs_genes.json")
-  system(cmd)
+library(openxlsx)
+library(data.table)
+dat <- as.data.frame(fread("cor/abs_genes.csv"))
+dat <- dat[,c("Pmid", "Doi", "Keywords", "Correlation")]
+write.xlsx(dat, "cor/abs_genes.xlsx")
 }
